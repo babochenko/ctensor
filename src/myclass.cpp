a42915b6fc0c0e7e7c3cf5ae2ae1c0fdb8d1e4ac
denisbabochenko <babochenko.denis2@gmail.com>
2024-08-14 01:18:04 +0100

    gitignore

--- src/myclass.cpp
+++ src/myclass.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include "myclass.h"
 
-void Portfolio::Portfolio(int value, std::string user_id) : value(value), user_id(user_id) {
+Portfolio::Portfolio(int value, std::string user_id) : value(value), user_id(user_id) {
   std::cout << "in constructor" << std::endl;
 }
 
